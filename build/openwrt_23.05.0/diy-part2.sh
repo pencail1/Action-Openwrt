dfce19cb3e31a9498d696c8521290da9f49c5041
PENCAIL\pencail <473632514@qq.com>
2024-04-24 10:41:01 +0800

    修改23.05.0

--- build/openwrt_23.05.0/diy-part2.sh
+++ build/openwrt_23.05.0/diy-part2.sh
@@ -13,9 +13,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-pushd feeds/packages/lang
-rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang
-popd
+#pushd feeds/packages/lang
+#rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang
+#popd
 
 
 #sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
